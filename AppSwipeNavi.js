1b4d67387169080a2fbed58d194c073d877b7761
kimjin0 <pkimjin0q@gmail.com>
2020-09-01 08:17:04 +0900

    배포

--- AppSwipeNavi.js
+++ AppSwipeNavi.js
@@ -1,15 +1,28 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import ListData from './AppFlatList';
-function HomeScreen() {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      {/* <Button title="view DisplayInfo" onPress={this.DisplayInfo} /> */}
     </View>
   );
 }
+function DisplayInfo() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>DisplayInfo Screen</Text>
+    </View>
+  );
+}
+
 function Settingscreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -24,7 +37,8 @@ function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={ListData} />
+        <Tab.Screen name="Settings" component={Settingscreen} />
+        <Tab.Screen name="List" component={ListData} />
       </Tab.Navigator>
     </NavigationContainer>
   );
