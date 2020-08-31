9662d55a7d120429e36346ed598eb9c8dd291277
kimjin0 <pkimjin0q@gmail.com>
2020-08-31 11:59:26 +0900

    top navi 적용

---
+++ AppSwipeNavi.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+function Settingscreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
+const Tab = createMaterialTopTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={Settingscreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+// https://reactnavigation.org/docs/hello-react-navigation
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './AppNavi';
+import App from './AppSwipeNavi';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
